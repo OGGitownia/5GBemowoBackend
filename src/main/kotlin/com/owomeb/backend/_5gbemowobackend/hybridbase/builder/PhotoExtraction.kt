5d0b48f4b4ed45a6df4dd3312c2dba3d0f9e06f7
Mikolaj <mkolodzi>
2025-04-23 21:33:27 +0200

    dodanie klasy PhotoExtraction

---
+++ src/main/kotlin/com/owomeb/backend/_5gbemowobackend/hybridbase/builder/PhotoExtraction.kt
@@ -0,0 +1,38 @@
+package com.owomeb.backend._5gbemowobackend.hybridbase.builder
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.owomeb.backend._5gbemowobackend.answering.Question
+import com.owomeb.backend._5gbemowobackend.answering.QuestionStatus
+import com.owomeb.backend._5gbemowobackend.architectureMasterpiece.PythonServerModel
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("/lamo_asker")
+class PhotoExtraction : PythonServerModel<PhotoExtraction.AugmentedQuery>(
+    scriptPath = "src/main/resources/pythonScripts/pythonServers/photoExtraction.py",
+    serverName = "unikalnanazwa",
+    autoClose = true
+) {
+
+    @PostMapping("/server-ready")
+    override fun markServerAsReady(@RequestBody body: Map<String, Any>): Map<String, String> {
+        return super.markServerAsReady(body)
+    }
+
+    fun extract(input: String, outputDocx: String, outputDir: String) {
+        val augmentedQuery = AugmentedQuery(input, outputDir, outputDocx)
+        this.addToQueue(augmentedQuery)
+    }
+
+    private val mapper = jacksonObjectMapper()
+
+    override fun publishResult(result: String, item: AugmentedQuery) {
+        println("print z metody published result na koniec dzialania ")
+
+    }
+
+
+    data class AugmentedQuery(val input: String, val outputDocx: String, val outputDir: String)
+}
